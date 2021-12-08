9cc27b398d8ebe7e9715f92789a3f4534a64d03e
Wiktor Chmielewski <wc429131@students.mimuw.edu.pl>
2021-12-08 17:24:00 +0100

    Dodano konstuktor klasy VirusGenealogy oraz funkcję get_stem_id

--- test.cc
+++ test.cc
@@ -1,6 +1,34 @@
 #include "virus_genealogy.h"
 using namespace std;
 
+#include <iterator>
+#include <iostream>
+
+template<typename T>
+requires bidirectional_iterator<T>
+void vectorBidirectional(T it, T end) {
+    for (; it != end; ++it) cout << *it;
+}
+
+class Virus {
+  public:
+    using id_type = std::string;
+    Virus(id_type const &_id) : id(_id) {
+    }
+    id_type get_id() const {
+      return id;
+    }
+  private:
+    id_type id;
+};
+
 int main() {
-    
+    vector<int> v({1, 2, 3});
+    vector<int>::iterator it = v.begin();
+    vector<int>::iterator end = v.end();
+    vectorBidirectional(it, end);
+
+    VirusGenealogy<Virus> gen("1");
+    gen.print("1");
+    cout << gen.get_stem_id() << endl;
 }
--- virus_genealogy.h
+++ virus_genealogy.h
@@ -3,17 +3,41 @@
 
 #include <vector>
 #include <memory>
+#include <map>
+#include <iostream> // do usunięcia
 
 template<typename Virus>
 class VirusGenealogy {
     private:
+        Virus::id_type stem_id;
         class Node {
             private:
-                Virus virus;
-                // powinniśmy przechowywać vector identyfikatorow
+                std::shared_ptr<Virus> virus_ptr;
+                std::vector<typename Virus::id_type> parents;
+                std::vector<std::shared_ptr<Virus>> children;
             public:
+                Node(const Virus::id_type &id) : virus_ptr(std::make_shared<Virus>(id)) {}
+                // funkcje do testowania
+                void printNode() {
+                    std::cout << virus_ptr->get_id() << std::endl;
+                    for (auto id : parents) std::cout << id << " ";
+                    std::cout << std::endl;
+                    for (auto ptr : children) std::cout << ptr->get_id() << " ";
+                    std::cout << std::endl;
+                }
         };
-        
+        std::map<typename Virus::id_type, Node> nodes;
+    public:
+        VirusGenealogy(Virus::id_type const &stem_id) : stem_id(stem_id) {
+            nodes.emplace(stem_id, Node(stem_id));
+        }
+        Virus::id_type get_stem_id() const {
+            return stem_id;
+        }
+        // funkcje do testowania
+        void print(Virus::id_type const &id) {
+            nodes.at(id).printNode();
+        }
 };
 
 #endif /* VIRUS_GENEALOGY_H */
